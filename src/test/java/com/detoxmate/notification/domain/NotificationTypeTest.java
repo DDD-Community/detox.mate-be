2df528e2a50231c0fe899e3eb6158401900be604
xeulbn <gsbtiger0215@gmail.com>
2026-04-15 22:15:59 +0900

    test:NotificationTypeTest 1차 작성

--- src/test/java/com/detoxmate/notification/domain/NotificationTypeTest.java
+++ src/test/java/com/detoxmate/notification/domain/NotificationTypeTest.java
@@ -3,6 +3,8 @@ package com.detoxmate.notification.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NotificationTypeTest {
@@ -11,30 +13,41 @@ class NotificationTypeTest {
     @DisplayName("알림 타입을 생성한다.")
     void createNotificationType(){
         //given
+        NotificationTypeCode typeCode = NotificationTypeCode.CERTIFICATION;
 
         //when
+        NotificationType notificationType = NotificationType.create(typeCode);
 
         //then
+        assertThat(notificationType.getTypeCode()).isEqualTo(NotificationTypeCode.CERTIFICATION);
     }
 
     @Test
-    @DisplayName("알림 타입은 정해진 ENUM값 만을 사용한다.")
-    void checkNotificationEnum(){
-        //given
-
-        //when
-
-        //then
+    @DisplayName("알림 타입 코드는 null일 수 없다.")
+    void notificationTypeCodeNotNull(){
+        //when&then
+        assertThatThrownBy(()->NotificationType.create(null))
+                .isInstanceOf(NotificationException.class)
+                .extracting(e->((NotificationException) e).getErrorCode())
+                .isEqualTo(NotificationErrorCode.INVALIDE_TYPE_CODE);
     }
 
     @Test
     @DisplayName("알림 타입과 일림은 연관관계이다.")
     void notificationTypeRelatedToNotification(){
         //given
+        NotificationTypeCode typeCode = NotificationTypeCode.COMMENT;
+        Notification notification = Notification.create(
+                "새로운 댓글!",
+                "{nickname}님이 댓글을 남겼습니다.",
+                typeCode
+        );
 
         //when
+        NotificationType notificationType = NotificationType.create(typeCode);
 
         //then
+        assertThat(notificationType.getTypeCode()).isEqualTo(notification.getTypeCode());
     }
 
 }
