e58a883a7efc888a4d72f0b25d33e345ee07e154
xeulbn <gsbtiger0215@gmail.com>
2026-04-15 22:30:43 +0900

    feat: NotificationType Domain구현

--- src/main/java/com/detoxmate/notification/domain/NotificationType.java
+++ src/main/java/com/detoxmate/notification/domain/NotificationType.java
@@ -1,5 +1,30 @@
 package com.detoxmate.notification.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notification_type")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NotificationType {
 
+    @Id
+    @Column(name = "notification_type_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "notification_type_name")
+    private NotificationTypeCode typeCode;
+
+    private NotificationType(NotificationTypeCode typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public static NotificationType create(NotificationTypeCode typeCode) {
+        return new NotificationType(typeCode);
+    }
 }
