94ae4fc007106c05d9a68800e8e798a194e32532
euijinkk <gytks4@naver.com>
2026-04-21 04:21:01 +0900

    feat: 유저 삭제 API 도 dev 용으로 분리

--- src/main/java/com/detoxmate/user/controller/UserDevController.java
+++ src/main/java/com/detoxmate/user/controller/UserDevController.java
@@ -1,26 +1,27 @@
 package com.detoxmate.user.controller;
 
 import com.detoxmate.common.AccessTokenExtractor;
-import com.detoxmate.user.dto.MyProfileResponse;
 import com.detoxmate.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Profile("dev")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-public class UserController {
+public class UserDevController {
     private final UserService userService;
 
-    @GetMapping("/me")
-    public MyProfileResponse getMe(
+    @DeleteMapping("/me")
+    public void withdraw(
             @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authorizationHeader
     ) {
         String accessToken = AccessTokenExtractor.require(authorizationHeader);
-        return userService.getMe(accessToken);
+        userService.withdraw(accessToken);
     }
 }
