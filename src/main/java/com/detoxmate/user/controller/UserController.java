32feeddf4996f4307b4f26dce3cb2c51c353df49
euijinkk <gytks4@naver.com>
2026-04-21 00:21:51 +0900

    refactor: AccessTokenExtractor 분리

--- src/main/java/com/detoxmate/user/controller/UserController.java
+++ src/main/java/com/detoxmate/user/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.detoxmate.user.controller;
 
+import com.detoxmate.common.AccessTokenExtractor;
 import com.detoxmate.user.dto.MyProfileResponse;
 import com.detoxmate.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/users")
@@ -22,7 +21,7 @@ public class UserController {
     public MyProfileResponse getMe(
             @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authorizationHeader
     ) {
-        String accessToken = requireAccessToken(authorizationHeader);
+        String accessToken = AccessTokenExtractor.require(authorizationHeader);
         return userService.getMe(accessToken);
     }
 
@@ -30,26 +29,7 @@ public class UserController {
     public void withdraw(
             @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String authorizationHeader
     ) {
-        String accessToken = requireAccessToken(authorizationHeader);
+        String accessToken = AccessTokenExtractor.require(authorizationHeader);
         userService.withdraw(accessToken);
-    }
-
-    private String requireAccessToken(String authorizationHeader) {
-        // Authorization 헤더 자체가 없으면 인증되지 않은 요청으로 본다.
-        if (authorizationHeader == null || authorizationHeader.isBlank()) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authorization header is required");
-        }
-
-        return extractAccessToken(authorizationHeader);
-    }
-
-    private String extractAccessToken(String authorizationHeader) {
-        String bearerPrefix = "Bearer ";
-
-        if (!authorizationHeader.startsWith(bearerPrefix)) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Bearer token is required");
-        }
-
-        return authorizationHeader.substring(bearerPrefix.length());
     }
 }
