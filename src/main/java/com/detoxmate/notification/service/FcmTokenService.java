4407370e3682e828ad2f70fc44b264447925a8f7
xeulbn <gsbtiger0215@gmail.com>
2026-04-22 14:08:23 +0900

    feat:FcmTokenService & refactor:FcmTokenServiceTest

--- src/main/java/com/detoxmate/notification/service/FcmTokenService.java
+++ src/main/java/com/detoxmate/notification/service/FcmTokenService.java
@@ -1,5 +1,27 @@
 package com.detoxmate.notification.service;
 
+import com.detoxmate.notification.domain.DevicePlatform;
+import com.detoxmate.notification.domain.FcmToken;
+import com.detoxmate.notification.repository.FcmTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class FcmTokenService {
+
+    private final FcmTokenRepository fcmTokenRepository;
+
+    @Transactional
+    public FcmToken register (Long userId, String token, DevicePlatform devicePlatform) {
+        fcmTokenRepository.deleteByToken(token);
+        return fcmTokenRepository.save(FcmToken.create(userId, token, devicePlatform));
+    }
+
+    @Transactional
+    public void remove(String token) {
+        fcmTokenRepository.deleteByToken(token);
+    }
 
 }
--- src/test/java/com/detoxmate/notification/service/FcmTokenServiceTest.java
+++ src/test/java/com/detoxmate/notification/service/FcmTokenServiceTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
@@ -20,7 +20,7 @@ import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 class FcmTokenServiceTest {
 
-    @MockitoBean
+    @Mock
     private FcmTokenRepository fcmTokenRepository;
 
     @InjectMocks
