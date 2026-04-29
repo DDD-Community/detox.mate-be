7bf74f0d551504220a08be18c7a38d6941281a17
xeulbn <gsbtiger0215@gmail.com>
2026-04-30 00:26:24 +0900

    feat : activity record challenge status domain

--- src/main/java/com/detoxmate/activityrecordchallengestatus/domain/ActivityRecordChallengeStatus.java
+++ src/main/java/com/detoxmate/activityrecordchallengestatus/domain/ActivityRecordChallengeStatus.java
@@ -1,4 +1,63 @@
 package com.detoxmate.activityrecordchallengestatus.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "activity_record_challenge_status")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ActivityRecordChallengeStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "activity_record_challenge_status_id")
+    private int id;
+
+    @Column(name = "group_challenge_id", nullable = false)
+    private Long groupChallengeId;
+
+    @Column(name = "activity_record_id", nullable = false)
+    private Long activityRecordId;
+
+    @Column(name = "comment_count", nullable = false)
+    private int commentCount;
+
+    @Column(name = "reaction_count", nullable = false)
+    private int reactionCount;
+
+    @Column(name = "poke_count", nullable = false)
+    private int pokeCount;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private ActivityRecordChallengeStatus(Long groupChallengeId, Long activityRecordId) {
+        this.groupChallengeId = groupChallengeId;
+        this.activityRecordId = activityRecordId;
+        this.commentCount = 0;
+        this.reactionCount = 0;
+        this.pokeCount = 0;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public static ActivityRecordChallengeStatus create(Long groupChallengeId, Long activityRecordId) {
+        return new ActivityRecordChallengeStatus(groupChallengeId, activityRecordId);
+    }
+
+    public void increaseCommentCount() {
+        this.commentCount++;
+    }
+
+    public void increaseReactionCount() {
+        this.reactionCount++;
+    }
+
+    public void increasePokeCount() {
+        this.pokeCount++;
+    }
 }
