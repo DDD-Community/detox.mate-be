446549baca4e3e30b924c18c5ce8c878a38462e9
xeulbn <gsbtiger0215@gmail.com>
2026-04-30 00:16:56 +0900

    feat: pokeController

--- src/main/java/com/detoxmate/poke/controller/PokeController.java
+++ src/main/java/com/detoxmate/poke/controller/PokeController.java
@@ -3,6 +3,7 @@ package com.detoxmate.poke.controller;
 import com.detoxmate.auth.CurrentUser;
 import com.detoxmate.poke.service.PokeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/group-challenges")
+@Slf4j
 public class PokeController {
 
     private final PokeService pokeService;
@@ -19,8 +21,9 @@ public class PokeController {
     @PostMapping("/{groupChallengeId}/members/{targetUserId}/poke")
     public ResponseEntity<Void> pokeUser(@PathVariable Long groupChallengeId,
                                          @PathVariable Long targetUserId,
-                                         CurrentUser currentUser
-    ) {
+                                         CurrentUser currentUser) {
+
+        log.info("[Poke][create-poke] senderUserId={} poked receiverUserId={} in groupChallengeId={}", currentUser.id(), targetUserId, groupChallengeId);
         pokeService.poke(groupChallengeId, targetUserId, currentUser.id());
         return ResponseEntity.noContent().build();
     }
