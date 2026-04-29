626b9dfd9593da9c262fcf6b337d1b7c528b1ba6
xeulbn <gsbtiger0215@gmail.com>
2026-04-29 10:59:53 +0900

    feat : CommentRepository

--- src/test/java/com/detoxmate/comment/repository/CommentRepositoryTest.java
+++ src/test/java/com/detoxmate/comment/repository/CommentRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class CommentRepositoryTest {
 
     @Autowired
